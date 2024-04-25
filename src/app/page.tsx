f225d012edde9c027af9f52a5005e28e82df98c5
ArpitaGanatra <ganatraarpita@gmail.com>
2024-04-25 10:18:56 +0530

    feat: added squares

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { Button } from "@squaress/ui";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Coming Soon...
+      <Button variant="primary">stake</Button>
     </main>
   );
 }
