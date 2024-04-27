67ab2ed1c714d4294d6971dfe337254f0eceaf4f
ArpitaGanatra <ganatraarpita@gmail.com>
2024-04-27 09:49:08 +0530

    feat: staking options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,58 @@ import {
   SegmentContainer,
   Text,
   InputField,
+  CustomRadioButton,
 } from "@/lib/ui";
 
 import { useState } from "react";
 import Logo from "./components/logo";
 import Icon from "@squaress/ui/icons";
 import Image from "next/image";
+// import { CustomRadioButton } from "./components/radio-group";
+
+const radioOptions = [
+  {
+    id: "liquid-stake",
+    value: "liquid-stake",
+    label: (
+      <div className="flex w-full justify-between">
+        <div className="flex flex-col gap-[2px]">
+          <Text className="l1-heavy">Liquid Stake</Text>
+          <Text className="l2-light">Unlock stake liquidity for DeFi</Text>
+        </div>
+        <div className="flex flex-col gap-[2px]">
+          <Text className="b4-light">APY</Text>
+          <Text className="l1-heavy">-</Text>
+        </div>
+      </div>
+    ),
+  },
+  {
+    id: "native-stake",
+    value: "native-stake",
+    label: (
+      <div className="flex w-full justify-between">
+        <div className="flex flex-col gap-[2px]">
+          <Text className="l1-heavy">Native Stake</Text>
+          <Text className="l2-light">Stake natively with ice validator</Text>
+        </div>
+        <div className="flex flex-col gap-[2px]">
+          <Text className="b4-light">APY</Text>
+          <Text className="l1-heavy">7.46%</Text>
+        </div>
+      </div>
+    ),
+  },
+];
 
 export default function Home() {
   const [stakeState, setStakeState] = useState("stake");
+  const [value, setValue] = useState("liquid-stake");
+  const [expandSetting, setExpandSetting] = useState(false);
+
+  console.log("vale", value);
+  //imagedelivery.net/rWTckr21FEHs39XCNFz7Yw/66c41648-d23e-4bee-45df-5ec4ecce5200/public
+
   return (
     <div className="m-16 w-full">
       <div className="flex gap-12">
@@ -69,7 +112,15 @@ export default function Home() {
               <div className="w-full flex flex-col gap-3">
                 <div className="flex justify-between w-full">
                   <div className="flex gap-[10px]">
-                    <Logo />
+                    <div className="flex-shrink-0">
+                      <Image
+                        src="https://imagedelivery.net/rWTckr21FEHs39XCNFz7Yw/66c41648-d23e-4bee-45df-5ec4ecce5200/public"
+                        alt="ice sol logo"
+                        className="rounded-full w-full"
+                        height={40}
+                        width={40}
+                      />
+                    </div>
                     <div>
                       <Text className="h5">SOL</Text>
                       <Text className="b3-light" color="secondary">
@@ -104,38 +155,41 @@ export default function Home() {
                 </Text>
               </div>
 
-              <div className="bg-color-surface-primary-transparent rounded-full flex items-center justify-center p-2">
-                <Icon
-                  name="arrow-down"
-                  color="#666666"
-                  height={14}
-                  width={14}
-                />
-              </div>
-
-              <div className="flex justify-between w-full">
-                <div className="flex gap-[10px]">
-                  <div className="flex-shrink-0">
-                    <Image
-                      src="https://imagedelivery.net/rWTckr21FEHs39XCNFz7Yw/14ecae6e-2c9f-448d-d695-a1853550e200/public"
-                      alt="ice sol logo"
-                      className="rounded-full w-full"
-                      height={40}
-                      width={40}
+              {value === "liquid-stake" && (
+                <>
+                  <div className="bg-color-surface-primary-transparent rounded-full flex items-center justify-center p-2">
+                    <Icon
+                      name="arrow-down"
+                      color="#666666"
+                      height={14}
+                      width={14}
                     />
                   </div>
+                  <div className="flex justify-between w-full">
+                    <div className="flex gap-[10px]">
+                      <div className="flex-shrink-0">
+                        <Image
+                          src="https://imagedelivery.net/rWTckr21FEHs39XCNFz7Yw/14ecae6e-2c9f-448d-d695-a1853550e200/public"
+                          alt="ice sol logo"
+                          className="rounded-full w-full"
+                          height={40}
+                          width={40}
+                        />
+                      </div>
 
-                  <div>
-                    <Text className="h5">iceSOL</Text>
-                    <Text className="b3-light" color="secondary">
-                      Recieve $iceSOL
-                    </Text>
+                      <div>
+                        <Text className="h5">iceSOL</Text>
+                        <Text className="b3-light" color="secondary">
+                          Recieve $iceSOL
+                        </Text>
+                      </div>
+                    </div>
+                    <div>
+                      <Text className="h4">135.6</Text>
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <Text className="h4">135.6</Text>
-                </div>
-              </div>
+                </>
+              )}
             </div>
 
             <div className="w-full flex items-center flex-col justify-center gap-5">
@@ -147,14 +201,38 @@ export default function Home() {
                 <div className="flex justify-between w-full">
                   <Text className="b4">Stake Settings</Text>
                   <div className="bg-color-surface-primary-transparent rounded-full flex items-center justify-center p-1">
-                    <Icon
-                      name="chevron-right"
-                      color="#666666"
-                      height={14}
-                      width={14}
-                    />
+                    {expandSetting ? (
+                      <div onClick={() => setExpandSetting(!expandSetting)}>
+                        <Icon
+                          name="chevron-down"
+                          color="#666666"
+                          height={14}
+                          width={14}
+                        />
+                      </div>
+                    ) : (
+                      <div onClick={() => setExpandSetting(!expandSetting)}>
+                        <Icon
+                          name="chevron-right"
+                          color="#666666"
+                          height={14}
+                          width={14}
+                        />
+                      </div>
+                    )}
                   </div>
                 </div>
+
+                {expandSetting && (
+                  <CustomRadioButton
+                    options={radioOptions}
+                    defaultValue="liquid-stake"
+                    name="viewDensity"
+                    onChange={(newValue) => setValue(newValue)}
+                    className=""
+                    value={value}
+                  />
+                )}
               </div>
               <Button variant="primary" className="w-full">
                 Stake
